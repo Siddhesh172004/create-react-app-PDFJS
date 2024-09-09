20820d2ce7f092db427bca7b12cf5b0ec6d7fffa
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-09-09 20:55:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
             <div
                 style={{
                     height: 'auto',
-                    width: '100%',
+                    width: '70%',
                     
                 }}
             >
